2ec4f89346005549f6167111fe2150da10633baf
skem-skem <lazenda001@gmail.com>
2026-06-09 13:14:51 +0200

    chore: setup Prisma, PostgreSQL schema and dependencies

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: path.join("prisma", "schema.prisma"),
+  datasource: {
+    url: "postgresql://postgres:password@localhost:5432/tacticalstore",
+  },
+});
